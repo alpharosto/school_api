38ec3212869eb5d564793aa673fc0f55b28cd4c9
alpharosto <kharatsaurav2023@gmail.com>
2024-08-25 02:23:29 +0530

    deploy

--- config/db.js
+++ config/db.js
@@ -1,14 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'sql12.freesqldatabase.com',
-  user: 'sql12727635',
-  password: 'Q7sTEkH2Il',
-  database: 'sql12727635',
-  port : 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: 'localhost',
+  user: 'root',
+  password: 'asdf',
+  database: 'school_db'
 });
 
 db.connect((err) => {
